c47c2eefc26f7d8051c2eefe0f549516a2de054e
pyamsoft <developer.pyamsoft@gmail.com>
2020-11-17 22:16:49 -0800

    Break up the large function

--- highlander/src/main/java/com/pyamsoft/highlander/HighRunner.kt
+++ highlander/src/main/java/com/pyamsoft/highlander/HighRunner.kt
@@ -33,59 +33,81 @@ internal class HighRunner<T> internal constructor(private val logger: Logger) {
     private val mutex = Mutex()
     private var activeRunner: Runner<T>? = null
 
-    suspend fun run(scope: CoroutineScope, block: suspend CoroutineScope.() -> T): T {
-        // We must claim the mutex before checking task status because another task running in parallel
-        // could be changing the activeTask value
-        mutex.withLock {
-            logger.log { "Checking for active task" }
-            activeRunner?.also { active ->
-                // Cancel if already running.
-                val id = active.id
-                val task = active.task
-                logger.log { "Cancel active task: $id" }
-                task.cancelAndJoin()
-            }
+    /**
+     * We must claim the mutex before checking task status because another task running in parallel
+     * could be changing the activeTask value
+     */
+    private suspend fun cancelExistingTask() = mutex.withLock {
+        logger.log { "Checking for active task" }
+        activeRunner?.also { active ->
+            // Cancel if already running.
+            val id = active.id
+            val task = active.task
+            logger.log { "Cancel active task: $id" }
+            task.cancelAndJoin()
+        }
+    }
+
+    /**
+     * Claim the lock and look for who's the active runner
+     */
+    private suspend inline fun createNewTask(
+        scope: CoroutineScope,
+        crossinline block: suspend CoroutineScope.() -> T
+    ): Runner<T> = mutex.withLock {
+        val active = activeRunner
+        if (active != null) {
+            logger.log { "Found existing task, cancel: ${active.id}" }
+            active.task.cancelAndJoin()
         }
 
-        // Claim the lock and look for who's the active runner
-        val runner = mutex.withLock {
-            val active = activeRunner
-            if (active != null) {
-                logger.log { "Found existing task, cancel: ${active.id}" }
-                active.task.cancelAndJoin()
-            }
+        val currentId = randomId()
+        val newTask = scope.async(start = CoroutineStart.LAZY) { block() }
+        val newRunner = Runner(currentId, newTask)
+        activeRunner = newRunner
+        logger.log { "Marking task as active: $currentId" }
+        return@withLock newRunner
+    }
 
-            val currentId = randomId()
-            val newTask = scope.async(start = CoroutineStart.LAZY) { block() }
-            val newRunner = Runner(currentId, newTask)
-            activeRunner = newRunner
-            logger.log { "Marking task as active: $currentId" }
-            return@withLock newRunner
-        }
+    /**
+     * Await the completion of the task
+     */
+    @CheckResult
+    private suspend fun runTask(runner: Runner<T>): T {
+        logger.log { "Awaiting task ${runner.id}" }
+        val result = runner.task.await()
+        logger.log { "Completed task ${runner.id}" }
+        return result
+    }
 
-        // Await the completion of the task
-        try {
-            logger.log { "Awaiting task ${runner.id}" }
-            val result = runner.task.await()
-            logger.log { "Completed task ${runner.id}" }
-            return result
-        } finally {
-            // Make sure the activeTask is actually us, otherwise we don't need to do anything
-            // Fast path in this case only since we have the id to guard with as well as the state
-            // of activeTask
-            if (activeRunner?.id == runner.id) {
-                // Run in the NonCancellable context because the mutex must be claimed to free the activeTask
-                // or else we will leak memory.
-                withContext(context = NonCancellable) {
-                    mutex.withLock {
-                        // Check again to make sure we really are the active task
-                        if (activeRunner?.id == runner.id) {
-                            logger.log { "Releasing activeTask ${runner.id} since it is complete" }
-                            activeRunner = null
-                        }
+    /**
+     * Make sure the activeTask is actually us, otherwise we don't need to do anything
+     * Fast path in this case only since we have the id to guard with as well as the state
+     * of activeTask
+     */
+    private suspend fun clearActiveTask(runner: Runner<T>) {
+        if (activeRunner?.id == runner.id) {
+            // Run in the NonCancellable context because the mutex must be claimed to free the activeTask
+            // or else we will leak memory.
+            withContext(context = NonCancellable) {
+                mutex.withLock {
+                    // Check again to make sure we really are the active task
+                    if (activeRunner?.id == runner.id) {
+                        logger.log { "Releasing activeTask ${runner.id} since it is complete" }
+                        activeRunner = null
                     }
                 }
             }
+        }
+    }
+
+    suspend fun run(scope: CoroutineScope, block: suspend CoroutineScope.() -> T): T {
+        cancelExistingTask()
+        val runner = createNewTask(scope, block)
+        return try {
+            runTask(runner)
+        } finally {
+            clearActiveTask(runner)
         }
     }
 
